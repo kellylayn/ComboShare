96b0d5c96c43218176aa345e5a9b956e84dd9a19
kellylayn <kellyrabbit1@gmail.com>
2023-04-06 06:36:52 +0000

    AI機能の導入、管理者側機能の作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,23 @@
 Rails.application.routes.draw do
-
   #ゲストログイン機能
   devise_scope :end_user do
     post 'end_users/guest_sign_in', to: 'end_users/sessions#guest_sign_in'
   end
 
+  #管理者ログイン機能
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
+  sessions: "admin/sessions"
+  }
 
+  #利用者ログイン機能
+  devise_for :end_users, skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+  }
+
+
+  #利用者側ルーティング
   scope module: :public do
-    devise_for :end_users, skip: [:passwords], controllers: {
-    registrations: "public/registrations",
-    sessions: 'public/sessions'
-    }
     root to: 'homes#top'
     get '/homes/about' => 'homes#about', as: 'about'
     get '/homes/privacy_policy' => 'homes#privacy_policy', as: 'privacy_policy'
@@ -24,10 +31,10 @@ Rails.application.routes.draw do
   end
 
 
+  #管理者側ルーティング
   namespace :admin do
-    devise_for :admin, skip: [:registrations, :passwords], controllers: {
-    sessions: "admin/sessions"
-    }
-    root to: 'homes#top'
+    root to: 'end_users#index'
+    resources :end_users, only: [:edit, :destroy]
+    resources :articles, only: [:index, :show, :destroy]
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Admin.create!(
+  email: 'combo@share',
+  password: 'mimimi'
+)
+
 EndUser.create!(
   name: "パなし昇竜はアド",
   email: "panasi@syouryuu",
